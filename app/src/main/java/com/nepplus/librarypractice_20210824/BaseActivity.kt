8b071a8e9e07da2a6524c019cefe11223819334f
이재환 <qorvhfl080@naver.com>
2021-08-24 15:16:12 +0900

    액티비티 onCreate 정리용 클래스 : BaseActivity 추가

---
+++ app/src/main/java/com/nepplus/librarypractice_20210824/BaseActivity.kt
@@ -0,0 +1,16 @@
+package com.nepplus.librarypractice_20210824
+
+import androidx.appcompat.app.AppCompatActivity
+
+abstract class BaseActivity : AppCompatActivity() {
+
+//    this 자리에 대신 들어갈 변수
+    val mContext = this
+
+//    이벤트 처리 관련 코드를 모아줄 함수
+    abstract fun setupEvents()
+
+//    값을 띄우는 코드를 모아줄 함수
+    abstract fun setValues()
+
+}
