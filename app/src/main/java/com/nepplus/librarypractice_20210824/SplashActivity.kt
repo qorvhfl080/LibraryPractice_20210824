8c0c308aa5afb26387e068234bcc0a1ace7f7966
이재환 <qorvhfl080@naver.com>
2021-08-24 17:50:34 +0900

    스플래시 액티비티에서 메인 액티비티로 2.5초 후에 이동 추가

--- app/src/main/java/com/nepplus/librarypractice_20210824/SplashActivity.kt
+++ app/src/main/java/com/nepplus/librarypractice_20210824/SplashActivity.kt
@@ -1,7 +1,10 @@
 package com.nepplus.librarypractice_20210824
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 
 class SplashActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,7 +15,12 @@ class SplashActivity : BaseActivity() {
     }
 
     override fun setupEvents() {
-
+        val loadingHandler = Handler(Looper.getMainLooper())
+        loadingHandler.postDelayed( {
+            val intent = Intent(mContext, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 2500)
     }
 
     override fun setValues() {
